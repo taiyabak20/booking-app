e19ce097da7736a6a3cc18c43af5761d74fc7e9c
taiyabak20 <taiyabak20@gmail.com>
2023-08-15 17:26:13 +0530

    showing the data in screen

--- booking app/main.js
+++ booking app/main.js
@@ -1,26 +1,41 @@
 
-const nameInput = document.querySelector('#name');
-const emailInput= document.querySelector('#email')
 
 
-
+let list= document.querySelector('#users').innerHTML;
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', e => {
+
   e.preventDefault();
   //console.log(nameInput.value);
   //console.log(emailInput.value);
+let nameInput = document.querySelector('#name').value;
+let emailInput= document.querySelector('#email').value;
 
+
+  const storedUserDataJSON = localStorage.getItem("details");
+  const details = storedUserDataJSON ? JSON.parse(storedUserDataJSON) : [];
+
+  
   const objInput = {
-    name: nameInput.value ,
-    email: emailInput.value
+    name: nameInput ,
+    email: emailInput
   };
 
+details.push(objInput);
 
-  localStorage.setItem("userInput", JSON.stringify(objInput));
+console.log(details);
+  localStorage.setItem("details", JSON.stringify(details));
+
+  let print =''
+  details.forEach(user => {
+    print+=`&bull; ${user.name} - ${user.email}  <br>`
+    document.querySelector('#users').innerHTML=print;
+  });
   
-  nameInput.value='';
-  emailInput.value='';
+  document.querySelector('#name').value = '';
+  document.querySelector('#email').value = '';
 
-  console.log(objInput);
+  //console.log(objInput);
 });
+console.log(list);
