ceee2145bb829eea6608e1d645eeaad7552564ae
taiyabak20 <taiyabak20@gmail.com>
2023-08-15 14:23:41 +0530

    user data as object

--- booking app/main.js
+++ booking app/main.js
@@ -2,15 +2,25 @@
 const nameInput = document.querySelector('#name');
 const emailInput= document.querySelector('#email')
 
+
+
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', e => {
   e.preventDefault();
   //console.log(nameInput.value);
   //console.log(emailInput.value);
-  localStorage.setItem("email", `${emailInput.value}`);
-  localStorage.setItem("name", `${nameInput.value}`);
+
+  const objInput = {
+    name: nameInput.value ,
+    email: emailInput.value
+  };
+
+
+  localStorage.setItem("userInput", JSON.stringify(objInput));
+  
   nameInput.value='';
   emailInput.value='';
- 
+
+  console.log(objInput);
 });
