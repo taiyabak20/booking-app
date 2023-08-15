ccbaaa26ed6629d6d754111d83d171780deb0956
taiyabak20 <taiyabak20@gmail.com>
2023-08-15 18:23:54 +0530

    add delete button

--- booking app/main.js
+++ booking app/main.js
@@ -26,11 +26,31 @@ details.push(objInput);
 console.log(details);
   localStorage.setItem("details", JSON.stringify(details));
 
-  let print =''
-  details.forEach(user => {
-    print+=`&bull; ${user.name} - ${user.email}  <br>`
-    document.querySelector('#users').innerHTML=print;
-  });
+
+  function updateUsersList() {
+    let print = '';
+    details.forEach((user, index) => {
+      print += `&bull; ${user.name} - ${user.email} <button class="dlt-button">delete</button><br>`;
+    });
+  
+    document.querySelector('#users').innerHTML = print;
+  
+    let deleteButtons = document.querySelectorAll(".dlt-button");
+  
+    deleteButtons.forEach((deleteButton, index) => {
+      deleteButton.addEventListener("click", () => {
+        deleteUser(index);
+      });
+    });
+  }
+  
+  updateUsersList(); 
+
+  function deleteUser(index) {
+    details.splice(index, 1);
+    updateUsersList();
+    localStorage.setItem("details", JSON.stringify(details)); 
+  }
   
   document.querySelector('#name').value = '';
   document.querySelector('#email').value = '';
